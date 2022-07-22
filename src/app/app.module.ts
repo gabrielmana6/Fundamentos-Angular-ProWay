ac89bb75d2ca669b90870721ecc84bf5e834cb34
Gabriel Manassés <gabrielmanasses13@gmail.com>
2022-07-22 07:24:14 -0300

    Primeiro commit Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.components';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicaPorPipe } from './multiplicar-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent,
+    MultiplicaPorPipe,
+    PipeCustomizadoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/multiplicar-por.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { MultiplicaPorPipe } from './multiplicar-por.pipe';
+
+describe('MultiplicaPorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new MultiplicaPorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
